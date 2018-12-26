64eb7e0eeb768f9fcd5217d95b5f33aaea4448cd
Gerrard Lindsay <gerrard.lindsay@adp.com>
2018-12-25 23:14:07 -0500

    Basic handling of a round

--- src/spades-game.ts
+++ src/spades-game.ts
@@ -1,9 +1,96 @@
 import { Card, PlayerHand, dealCards } from './cards';
 
+export enum RoundState {
+  NotStarted = 0,
+  InProgress,
+  Completed,
+};
+
+export class Round {
+  // TODO: suite should really be an enum
+  constructor(roundNumber: number, startingPlayerIndex: number) {
+    this.roundNumber = roundNumber;
+    this.playCount = 0;
+    this.currentPlayerIndex = startingPlayerIndex;
+    this.suit = -1;
+    this.roundComplete = false;
+    this.currentMaxCard = null;
+    this.winner = null;
+    this.state = RoundState.NotStarted;
+  }
+  roundNumber: number;
+  playCount: number;
+  currentPlayerIndex: number;
+  suit: number;
+  roundComplete: boolean;
+  currentMaxCard: Card | null;
+  winner: number | null;
+  state: RoundState;
+};
+
 export class SpadesGame {
   constructor() {
+    this.rounds = [];
+    this.playerHands = [];
+  }
+
+  startGame(): Round {
     this.playerHands = dealCards();
-    this.currentRound = 0;
+    const firstRound = new Round(0, 0);
+    this.rounds.push(firstRound);
+
+    return firstRound;
+  }
+
+  playCard(playerHandIndex: number, c: Card) {
+    const currentRound = this.rounds[this.rounds.length - 1];
+    // TODO: remove the card from the player's hand...return it?
+
+    const updatedRoundObject = {
+      ...currentRound, ...{
+      playCount: currentRound.playCount + 1,
+      state: (currentRound.playCount < 2)
+        ? RoundState.InProgress
+        : RoundState.Completed,
+    }};
+    this.rounds[this.rounds.length - 1] = updatedRoundObject;
+
+    if (currentRound.playCount === 0) {
+      updatedRoundObject.suit = c.suit;
+      updatedRoundObject.currentMaxCard = c;
+      updatedRoundObject.winner = playerHandIndex;
+      // console.log(JSON.stringify(updatedRoundObject, null, 2));
+      return updatedRoundObject;
+    }
+
+    const maxCard = currentRound.currentMaxCard!;
+
+    if (updatedRoundObject.playCount === 3) {
+      updatedRoundObject.state = RoundState.Completed;
+    }
+
+    if (c.suit === maxCard.suit && c.rank > maxCard.rank) {
+      console.log(`${c.getName()} beats ${maxCard.getName()}`);
+      updatedRoundObject.currentMaxCard = c;
+      updatedRoundObject.winner = playerHandIndex;
+      // console.log(JSON.stringify(updatedRoundObject, null, 2));
+      return updatedRoundObject;
+    }
+
+    // TODO: magic number
+    // spades beats non-spades
+    if (maxCard.suit !== 3 && c.suit === 3) {
+      console.log(`Spade ${c.getName()} beats non-spade ${maxCard.getName()}`);
+      updatedRoundObject.currentMaxCard = c;
+      updatedRoundObject.winner = playerHandIndex;
+      // console.log(JSON.stringify(updatedRoundObject, null, 2));
+      return updatedRoundObject;
+    }
+
+    // TODO: was it the last play of this round?
+    // TODO: debug
+    // console.log(JSON.stringify(updatedRoundObject, null, 2));
+    return updatedRoundObject;
   }
 
   showHands(): void {
@@ -15,6 +102,6 @@ export class SpadesGame {
   }
 
   playerHands: PlayerHand[];
-  currentRound: number;
+  rounds: Round[];
  }
 
--- tests/spades-game.test.ts
+++ tests/spades-game.test.ts
@@ -1,7 +1,26 @@
-import { SpadesGame } from '../src/spades-game';
+import { SpadesGame, RoundState } from '../src/spades-game';
 
+// TODO: should be mocking the deal to hardcode a sequence for testing
 test('basic', () => {
   const game = new SpadesGame();
+  let currentRound = game.startGame();
 
   expect(game.playerHands.length).toBe(4);
+
+  const firstPlayerCard = game.playerHands[0].cards[7];
+  console.log(`First player will play ${firstPlayerCard.getName()}`);
+
+  currentRound = game.playCard(0, firstPlayerCard);
+
+  for (let i = 1; i < 4; i++) {
+    const currentPlayerCard =
+      game.playerHands[i].cards.find(c => c.suit === currentRound.suit)
+        || game.playerHands[i].cards[0];
+
+    console.log(`${['Second', 'Third', 'Fourth'][i - 1]}  player will play ${currentPlayerCard.getName()}`);
+    currentRound = game.playCard(i, currentPlayerCard!);
+  }
+
+  expect(currentRound.state).toBe(RoundState.Completed);
+  console.log(`The winner was ${['First', 'Second', 'Third', 'Fourth'][currentRound.winner!]} player with ${currentRound.currentMaxCard!.getName()}`);
 });
